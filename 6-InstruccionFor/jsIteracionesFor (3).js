a2416f3207a36e2dca62ef1d01c49959efc8b0ab
hernangiacobini <36521542+hernangiacobini@users.noreply.github.com>
2018-02-23 22:16:52 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,11 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
 
-
+	for(var i=0;i<repeticiones;i++)
+	{
+		console.log("Hola UTN FRA" + i);
+	}
 
 }//FIN DE LA FUNCIÓN
